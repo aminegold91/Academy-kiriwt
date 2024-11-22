01abd9332b7587983d7dddcc3b67cf2873da6780
Lamine el kouss <aminegold91@gmail.com>
2024-11-22 19:54:30 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+  //  التحقق من تسجيل الدخول
+    if (localStorage.getItem("isLoggedIn") !== "true") {
+      alert("يرجى تسجيل الدخول للوصول إلى هذه الصفحة.");
+      window.location.href = "login.html"; // إعادة التوجيه إلى صفحة تسجيل الدخول
+    }
+
+
+
+
+    const currentUser = localStorage.getItem("currentUser");
+    if (currentUser) {
+        document.getElementById("username").textContent = currentUser;
+    }
+
+
+
+    document.getElementById("logoutButton").addEventListener("click", function () {
+        // إزالة حالة تسجيل الدخول
+        localStorage.removeItem("isLoggedIn");
+        localStorage.removeItem("currentUser");
+        alert("تم تسجيل الخروج بنجاح.");
+        window.location.href = "login.html"; // إعادة التوجيه إلى صفحة تسجيل الدخول
+    });
